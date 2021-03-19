dd1f577888a2f0d1712019734aba7b1426f12215
Sergey <belyy.s.o16@gmail.com>
2021-03-19 18:27:19 +0300

    Allure homework don`t work

---
+++ src/test/java/cucumber/CucumberSteps.java
@@ -0,0 +1,136 @@
+package cucumber;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Пусть;
+import io.cucumber.java.ru.Тогда;
+import io.cucumber.java.ParameterType;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class CucumberSteps{
+
+    private static WebDriver driver;
+    private static WebDriverWait wait;
+    private static final By searchForm =  By.cssSelector("select[data-marker='search-form/category']");
+    private static final String URL =  "https://www.avito.ru/";
+    private static final By searchFormSuggest =  By.cssSelector("input[data-marker='search-form/suggest']");
+    private static final By searchFormRegion =  By.cssSelector("div[data-marker='search-form/region']");
+    private static final By popupLocation =  By.cssSelector("input[data-marker='popup-location/region/input']");
+    private static final By searchCity =  By.xpath("//strong[contains(text(),'Владивосток')]");
+    private static final By popupLocationSaveButton =  By.cssSelector("button[data-marker='popup-location/save-button']");
+    private static final By popupLocationSubmitButton =  By.cssSelector("button[data-marker='search-form/submit-button']");
+    private static final By deliveryFilter =  By.cssSelector("label[data-marker='delivery-filter']");
+    private static final By searchFiltersSubmitButton = By.xpath("//button[@data-marker='search-filters/submit-button']");
+    private static final By option =  By.xpath("//option[contains(text(),'По умолчанию')] /..");
+    private static final By priceSelector =  By.xpath("//span[contains(text(),'₽')] /..");
+
+
+    @ParameterType(".*")
+    public CategoryItem categoryItem(String type){
+        return CategoryItem.valueOf(type);
+    }
+
+    @ParameterType(".*")
+    public PriceOrder priceOrder(String price){
+        return PriceOrder.valueOf(price);
+    }
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "src\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(800, TimeUnit.MILLISECONDS);
+    }
+
+    @Step
+    @Пусть("^открыт ресурс авито$")
+    public static void Open(){
+        driver.get(URL);
+    }
+
+    @Step
+    @И("в выпадающем списке категорий выбрана {categoryItem}")
+    public static void selevtOrg(CategoryItem categoryItem){
+        WebElement selectElem = driver.findElement(searchForm);
+        Select select = new Select(selectElem);
+        select.selectByValue(categoryItem.value);
+    }
+
+    @Step
+    @И("^в поле поиска введено значение ([^\\\"]*)$")
+    public static void SearchSendKeys(String word){
+        driver.findElement(searchFormSuggest).sendKeys(word);
+    }
+
+    @Step
+    @Тогда("^кликнуть по выпадающему списку региона$")
+    public static void Region(){
+        driver.findElement(searchFormRegion).click();
+    }
+
+    @Step
+    @Тогда("^в поле регион введено значение ([^\\\"]*)$")
+    public static void chouseRegion(String city){
+        driver.findElement(popupLocation).sendKeys(city);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//strong[contains(text(),'"+ city +"')]"))).click();
+        driver.findElement(popupLocationSaveButton).click();
+    }
+
+    @Step
+    @И("^нажата кнопка показать объявления$")
+    public static void clickButtonSearch(){
+        driver.findElement(popupLocationSubmitButton).click();
+
+    }
+
+    @Step
+    @Тогда("^активирован чекбокс только с доставкой$")
+    public static void mail(){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, Math.max(document.documentElement.scrollHeight, document.body.scrollHeight, document.documentElement.clientHeight));");
+        driver.findElement(deliveryFilter).click();
+        driver.findElement(searchFiltersSubmitButton).click();
+    }
+
+    @Step
+    @И("в выпадающем списке сортировка выбрано значение {priceOrder}")
+    public static void expensive(PriceOrder priceOrder){
+        Select select = new Select(driver.findElement(option));
+        select.selectByValue(priceOrder.value);
+    }
+
+    @Step
+    @И("^в консоль выведено значение названия и цены (\\d+) первых товаров$")
+    public static void prices(int howMany){
+        List<WebElement> price = driver.findElements(priceSelector);
+        for(int i = 0;i<howMany;i++){
+            System.out.println(price.get(i).getText());
+
+        }
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+    }
+
+
+}
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,111 +1,58 @@
-import io.qameta.allure.Attachment;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import io.qameta.allure.Features;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.interactions.Locatable;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import ru.yandex.qatools.ashot.AShot;
-import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class test {
+public class test{
 
     private WebDriver driver;
     private WebDriverWait wait;
-    private final By searchForm =  By.cssSelector("select[data-marker='search-form/category']");
-    private final String URL =  "https://www.avito.ru/";
-    private final By searchFormSuggest =  By.cssSelector("input[data-marker='search-form/suggest']");
-    private final By searchFormRegion =  By.cssSelector("div[data-marker='search-form/region']");
-    private final By popupLocation =  By.cssSelector("input[data-marker='popup-location/region/input']");
-    private final By searchCity =  By.xpath("//strong[contains(text(),'Владивосток')]");
-    private final By popupLocationSaveButton =  By.cssSelector("button[data-marker='popup-location/save-button']");
-    private final By popupLocationSubmitButton =  By.cssSelector("button[data-marker='search-form/submit-button']");
-    private final By deliveryFilter =  By.cssSelector("label[data-marker='delivery-filter']");
-    private final By searchFiltersSubmitButton = By.xpath("//button[@data-marker='search-filters/submit-button']");
-    private final By option =  By.xpath("//option[contains(text(),'По умолчанию')] /..");
-    private final By priceSelector =  By.xpath("//span[contains(text(),'₽')] /..");
+    private WebDriverSteps steps;
 
-
-    @BeforeEach
+    @Before
     public void start() {
-        System.setProperty("webdriver.chrome.driver", "src\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, 10);
         driver.manage().timeouts().implicitlyWait(800, TimeUnit.MILLISECONDS);
-    }
-
-    @Test
-    public void AvitoCatalog() {
-        System.out.println();
-        driver.get(URL);
-
-        System.out.println("Открыть ресурс по адресу https://www.avito.ru/");
-        WebElement selectElem = driver.findElement(searchForm);
-        Select select = new Select(selectElem);
-        select.selectByValue("99");
-        captureScreenshot(driver);
-
-        System.out.println("Выбрать в выпадающем списке “категория”  значение оргтехника и расходники");
-        driver.findElement(searchFormSuggest).sendKeys("Принтер");
-        captureScreenshot(driver);
-
-        System.out.println("В поле поиск по объявлению ввести значение “Принтер”");
-        driver.findElement(searchFormRegion).click();
-        captureScreenshot(driver);
-
-        System.out.println("Нажать на поле город");
-        driver.findElement(popupLocation).sendKeys("Владивосток");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(searchCity)).click();
-        captureScreenshot(driver);
-
-        System.out.println("Заполнить значением “Владивосток” поле город  в открывшемся окне и кликнуть по первому предложенному варианту. Нажать на кнопку “Показать объявления”");
-        driver.findElement(popupLocationSaveButton).click();
-        driver.findElement(popupLocationSubmitButton).click();
-        captureScreenshot(driver);
-
-        System.out.println("Активировать чекбокс и нажать кнопку “Показать объявления”");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollTo(0, Math.max(document.documentElement.scrollHeight, document.body.scrollHeight, document.documentElement.clientHeight));");
-        driver.findElement(deliveryFilter).click();
-        driver.findElement(searchFiltersSubmitButton).click();
-        captureScreenshot(driver);
-
-        System.out.println("В выпадающем списке фильтрации выбрать фильтрацию по убыванию цены.");
-        select = new Select(driver.findElement(option));
-        select.selectByValue("2");
-        captureScreenshot(driver);
-
-        System.out.println("Вывести в консоль название и стоимость 3х самых дорогих принтеров");
-        List<WebElement> price = driver.findElements(priceSelector);
-        for(int i = 0;i<3;i++){
-            System.out.println(price.get(i).getText());
-        }
-
-
+        steps = new WebDriverSteps(driver,wait);
     }
 
 
-
-    @AfterEach
+    @After
     public void stop(){
         driver.quit();
         driver = null;
     }
 
-    @Attachment(value="Screenshot", type="image/png")
-    private static void captureScreenshot(WebDriver driver)
-    {
-        new AShot()
-                .shootingStrategy(ShootingStrategies.viewportPasting(100))
-                .takeScreenshot(driver);
-}
+    @Test
+    public void AvitoCatalog(){
+
+        steps.openAvito();
+
+        steps.selectCategory();
+
+        steps.find();
+
+        steps.city();
+
+        steps.Vladivostok();
+
+        steps.checkBox();
+
+        steps.delivery();
+
+        steps.priceChange();
+
+        steps.writePrices();
+
+    }
+
 }
 
